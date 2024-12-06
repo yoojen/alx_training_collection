0d4b99f72ac4fd2268e40e08026f8a6757900735
Eugene <eugenemutuyimana15@gmail.com>
2024-12-06 14:52:16 +0200

    python backend collection

---
+++ alx-backend-python-main/0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+    pytho basic annotation
+"""
+
+
+def add(a: float, b: float) -> float:
+    """return summation of two float numbers passsed from function"""
+    return a + b
---
+++ alx-backend-python-main/0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Python basic variables annotation"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """returns floor of float value """
+    return math.floor(n)
